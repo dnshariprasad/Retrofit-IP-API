e50c3d2b8ac30d6239a81e2803eaab1b60d3ed25
HariKvana <hari@kvana.com>
2016-02-17 23:10:21 +0530

    log added

--- app/src/main/java/com/basicretrofit/MainActivity.java
+++ app/src/main/java/com/basicretrofit/MainActivity.java
@@ -1,13 +1,15 @@
 package com.basicretrofit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,15 @@ public class MainActivity extends AppCompatActivity {
         RetrofitHandler.getInstance().getLocationInfo().enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-
+                if (response.isSuccess())
+                    Log.d(TAG, response.body());
+                else
+                    Log.e(TAG, response.message());
             }
 
             @Override
             public void onFailure(Call<String> call, Throwable t) {
-
+                Log.d(TAG, t.getLocalizedMessage());
             }
         });
     }
